f5f5208b5c323e80654d0c92bdb5403b31f14a43
Hugo Duval <hugo-duval-1@hotmail.fr>
2022-12-12 08:09:50 +0100

    added standard spring project architecture, as generated by intelliJ's IDEA

---
+++ src/main/java/com/example/bezkoderangularspringbootcrud/BezkoderAngularSpringBootCrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.bezkoderangularspringbootcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BezkoderAngularSpringBootCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BezkoderAngularSpringBootCrudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bezkoderangularspringbootcrud/BezkoderAngularSpringBootCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bezkoderangularspringbootcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BezkoderAngularSpringBootCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
