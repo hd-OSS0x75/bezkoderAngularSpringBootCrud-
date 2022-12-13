c0a3c5b88690626dd63900c12ce08c81c34bca53
Hugo Duval <hugo-duval-1@hotmail.fr>
2022-12-13 08:10:26 +0100

    added getter, setters & complete constructor to the tutorial model

--- src/main/java/com/example/bezkoderangularspringbootcrud/model/Tutorial.java
+++ src/main/java/com/example/bezkoderangularspringbootcrud/model/Tutorial.java
@@ -21,4 +21,43 @@ public class Tutorial {
     public Tutorial() {
 
     }
+
+    public Tutorial(String title, String description, boolean published) {
+        this.title = title;
+        this.description = description;
+        this.published = published;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isPublished() {
+        return published;
+    }
+
+    public void setPublished(boolean isPublished) {
+        this.published = isPublished;
+    }
+
+    @Override
+    public String toString() {
+        return "Tutorial [id=" + id + ", title=" + title + ", desc=" + description + ", published=" + published + "]";
+    }
 }
